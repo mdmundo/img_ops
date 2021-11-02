c3d37930710a233ce80c605d11d490fce086bbdb
Edmundo Paulino <manyymoore@gmail.com>
2021-11-02 11:19:19 +0000

    feat: ✨ Open and save image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use image::io::Reader;
+
 fn main() {
-    println!("Hello, world!");
+    let img = Reader::open("test_image.png")
+        .expect("Opening error")
+        .decode()
+        .expect("Decoding error");
+
+    // Operations
+
+    img.save("edited_test_image.png").expect("Saving error");
 }
